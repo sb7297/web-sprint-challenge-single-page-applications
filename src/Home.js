e8e44dc690413aaee17aecbd4af19b83deaf6c09
sb7297 <sbaireddy7297@gmail.com>
2021-07-23 11:17:35 -0400

    link from homepage to pizza order page

--- src/Home.js
+++ src/Home.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = function () {
-  return <p>Homepage</p>;
+  return (
+    <>
+      <p>Homepage</p>
+      <Link to="/pizza" id="order-pizza">Order a pizza here!</Link>
+    </>
+  );
 }
 
 export default Home;
